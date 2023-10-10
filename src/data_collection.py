26da92ab47b213e9d5f0a0bdf2b42e62109c82d5
davidadel66 <davidadel66@gmail.com>
2023-10-10 13:42:46 -0400

    added folders and started forecasting

---
+++ src/data_collection.py
@@ -0,0 +1,11 @@
+import yfinance as yf
+from datetime import datetime
+
+tsla = yf.Ticker('TSLA')
+
+end_date = datetime.now().strftime('%Y-%m-%d')
+hist_data = tsla.history(start='2022-01-01', end = end_date)
+
+hist_data.drop(columns=['Dividends', 'Stock Splits'], inplace=True)
+
+hist_data.to_csv('./data/tsla_daily.csv')
